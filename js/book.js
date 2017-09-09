e72cab58d05d7fabed3ace8b626ce9e281ed660b
MattZhu <matt4j@gmail.com>
2017-09-09 19:58:59 +0800

    add page 2

---
+++ js/book.js
@@ -0,0 +1,202 @@
+var units=[
+	{
+		title:"Unit 1 My classroom",
+		firstPage:2
+	},
+	{
+		title:"Unit 2 My schoolbag",
+		firstPage:12
+	},
+	{
+		title:"Unit 3 My friends",
+		firstPage:22
+	},
+	{
+		title:"Recycle 1",
+		firstPage:32
+	},
+	{
+		title:"Unit 4 My home",
+		firstPage:36
+	},
+	{
+		title:"Unit 5 Dinner's ready",
+		firstPage:46
+	},
+	{
+		title:"Unit 6 Meet my family",
+		firstPage:56
+	},
+	{
+		title:"Recycle 2",
+		firstPage:66
+	}
+]
+var pages=[
+	{
+		image:"front-cover.jpg",
+		audio:[]
+	},
+	{
+		image:"catalog.jpg",
+		audio:[]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-02.jpg",
+		audio:[
+			{
+				left:140,top:200,width:180,height:36,
+				src:'unit1/page-02_003.mp3'
+			},
+			{
+				left:310,top:307,width:190,height:40,
+				src:'unit1/page-02_004.mp3'
+			},
+			{
+				left:120,top:350,width:150,height:62,
+				src:'unit1/page-02_006.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-02_007.mp3'
+			},
+	
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-03.jpg",
+		audio:[
+			{
+				left:82,top:285,width:202,height:41,
+				src:'unit1/page-03_005.mp3'
+			},
+			{
+				left:304,top:120,width:182,height:60,
+				src:'unit1/page-03_008.mp3'
+			},
+			{
+				left:235,top:420,width:100,height:40,
+				src:'unit1/page-03_009.mp3'
+			},
+			{
+				left:318,top:516,width:94,height:40,
+				src:'unit1/page-03_010.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-04.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-05.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-06.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-07.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-08.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-09.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-10.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	},
+	{
+		unit:0,
+		image:"unit 1/page-11.jpg",
+		audio:[
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			},
+			{
+				left:140,top:570,width:198,height:40,
+				src:'unit1/page-03_007.mp3'
+			}
+		]
+	}
+	
+]
